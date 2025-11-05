3bc92bc0ddab7630d5339ee5853edc789ef40df4
Divyansh-Sarvaria <divyanshsarvaria@gmail.com>
2025-11-05 16:32:01 +0530

    3

---
+++ 2-how-node-works/starter/modules.js
@@ -0,0 +1,15 @@
+// console.log(arguments);
+
+// module.exports
+
+// const cal = require("./test-module-1");
+// const calc1 = new cal();
+// console.log(calc1.add(1, 4));
+
+// exports
+
+// const calc2 = require("./text-module-2");
+// console.log(calc2.add(3, 666));
+
+// cashing
+require("./test-module-3")();
---
+++ 2-how-node-works/starter/test-module-3.js
@@ -0,0 +1,3 @@
+console.log("hellow from the module");
+
+module.exports = () => console.log("log the text");
